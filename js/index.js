d0aaac775e9d6c852b6ec729f9ee960ecf264bc7
yigedabao <2431532802@qq.com>
2018-10-16 22:13:23 +0800

    第二天上传

--- js/index.js
+++ js/index.js
@@ -124,3 +124,64 @@ function autoPlay() {
 		oLaniamte();
 	}, 5000);
 }
+
+
+//遮罩弹出关闭
+//遮罩关闭
+$(".close").click(function(){
+	$("#zhezhao").css("display","none");
+})
+$(".close").mouseenter(function(){
+	$(this).css("cursor","pointer");
+})
+//遮罩弹出
+$("#subscription_btn").click(function(){
+	$("#zhezhao").css("display","block");
+})
+//表单正则验证
+function ze(obj,z){
+	$(".dy_btn").click(function(){
+		var re = z;
+		var res = obj.val();
+		if(!(re.test(res))){
+			$(".loader").html("请检查您的资料填写格式");
+			obj.css("border-color","red");
+		}else{
+			$(".loader").html("");
+			obj.css("border-color","#cccccc");
+			location.href="https://www.microsoftstore.com.cn/successfully-subscibed";
+		}
+	})
+}
+
+ze($(".name"),/\w/);
+ze($(".tel"),/1(3|5|7|8)[\d]{9}/);
+ze($(".email"),/^\w+@\w+\.[a-z]{2,3}(\.[a-z]{2,3})?$/);
+
+
+//登录
+$(".login_border").find("a").click(function(){
+	$(".loginContent").toggle(1);
+	$(".login_border").toggleClass("login_border_click");
+})
+$(".login").click(function(){
+	location.href = "login.html";
+})
+
+
+//创建账号页面点击转换
+$(".create_btn_1").click(function(){
+	$(".create_input_1").css("display","none");
+	$(".create_input_2").css("display","block");
+	$(".create_input_3").css("display","none");
+})
+$(".create_btn_2").click(function(){
+	$(".create_input_1").css("display","none");
+	$(".create_input_2").css("display","none");
+	$(".create_input_3").css("display","block");
+})
+$(".create_btn_3").click(function(){
+	$(".create_input_1").css("display","block");
+	$(".create_input_2").css("display","none");
+	$(".create_input_3").css("display","none");
+})
